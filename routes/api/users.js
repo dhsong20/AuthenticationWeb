3aa7d4c4e62b426816ae28b446f061b80ec92a4b
Dahoon Song <dhsong@berkeley.edu>
2019-04-19 08:08:12 -0700

    auth app

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,15 +3,13 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
-// Load input validation
+
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
-// Load User model
+
 const User = require("../../models/User");
 
-// @route POST api/users/register
-// @desc Register user
-// @access Public
+
 router.post("/register", (req, res) => {
   // Form validation
 const { errors, isValid } = validateRegisterInput(req.body);
@@ -43,9 +41,7 @@ const newUser = new User({
   });
 ;
 
-// @route POST api/users/login
-// @desc Login user and return JWT token
-// @access Public
+
 router.post("/login", (req, res) => {
   // Form validation
 const { errors, isValid } = validateLoginInput(req.body);
@@ -55,22 +51,19 @@ const { errors, isValid } = validateLoginInput(req.body);
   }
 const email = req.body.email;
   const password = req.body.password;
-// Find user by email
   User.findOne({ email }).then(user => {
-    // Check if user exists
     if (!user) {
       return res.status(404).json({ emailnotfound: "Email not found" });
     }
-// Check password
+
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        // User matched
-        // Create JWT Payload
+
         const payload = {
           id: user.id,
           name: user.name
         };
-// Sign token
+
         jwt.sign(
           payload,
           keys.secretOrKey,
--- server.js
+++ server.js
@@ -7,14 +7,12 @@ const users = require("./routes/api/users");
 
 const app = express();
 
-// Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
-// DB Config
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
@@ -35,5 +33,5 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
